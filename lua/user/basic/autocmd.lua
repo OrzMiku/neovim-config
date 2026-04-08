b9446b11c47c0cee33154f3e98f334c44127356e
OrzMiku <miku@ecy.pink>
2026-04-08 14:15:07 +0800

    use more mini.nvim's modules

--- lua/user/basic/autocmd.lua
+++ lua/user/basic/autocmd.lua
@@ -13,21 +13,6 @@ function M.setup()
     end,
   })
 
-  vim.api.nvim_create_autocmd('LspProgress', {
-    group = augroup 'UserLspProgress',
-    callback = function(ev)
-      local value = ev.data.params.value
-      vim.api.nvim_echo({ { value.message or 'done' } }, false, {
-        id = 'lsp.' .. ev.data.client_id,
-        kind = 'progress',
-        source = 'vim.lsp',
-        title = value.title,
-        status = value.kind ~= 'end' and 'running' or 'success',
-        percent = value.percentage,
-      })
-    end,
-  })
-
   vim.api.nvim_create_autocmd('LspAttach', {
     group = augroup 'UserLspAttach',
     callback = lsp_buf_setup,
--- lua/user/plugins/mini.lua
+++ lua/user/plugins/mini.lua
@@ -3,10 +3,21 @@ local M = {}
 function M.setup()
   require('mini.ai').setup {}
   require('mini.surround').setup {}
+  require('mini.bracketed').setup {}
   require('mini.icons').setup {}
   MiniIcons.mock_nvim_web_devicons()
   require('mini.statusline').setup {}
   require('mini.tabline').setup {}
+  require('mini.indentscope').setup {
+    draw = {
+      delay = 0,
+      animation = require('mini.indentscope').gen_animation.none(),
+    },
+  }
+  require('mini.pairs').setup {}
+  require('mini.cmdline').setup {}
+  require('mini.move').setup {}
+  require('mini.notify').setup {}
 end
 
 return M
