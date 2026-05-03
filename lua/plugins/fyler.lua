f1fd3da61f9865fc8f07b6241240e5bf57dfecbc
OrzMiku <miku@ecy.pink>
2026-05-03 15:56:28 +0800

    oil -> hyler

---
+++ lua/plugins/fyler.lua
@@ -0,0 +1,10 @@
+return {
+  'A7Lavinraj/fyler.nvim',
+  dependencies = { 'nvim-mini/mini.nvim' },
+  branch = 'stable',
+  lazy = false,
+  opts = {},
+  keys = {
+    { '<leader>e', '<cmd>Fyler<cr>', desc = 'Open File Explorer' },
+  },
+}
--- lua/plugins/oil.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'stevearc/oil.nvim',
-  opts = {},
-  keys = {
-    { '<leader>e', '<cmd>Oil<cr>', desc = 'Open parent directory' },
-  },
-}
