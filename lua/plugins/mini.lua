f771f1012039e3cb0499c05d782f4fc8267dd8af
OrzMiku <miku@ecy.pink>
2026-05-08 04:45:58 +0800

    mini files: change cwd

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -10,10 +10,29 @@ return {
       style = user_config.features.have_nerd_font and 'glyph' or 'ascii',
     }
     MiniIcons.mock_nvim_web_devicons()
+
     require('mini.files').setup {}
     vim.keymap.set('n', '<leader>e', function()
       MiniFiles.open()
     end, { desc = 'Open File Explorer' })
+
+    local set_cwd = function()
+      local path = (MiniFiles.get_fs_entry() or {}).path
+      if path == nil then
+        return vim.notify 'Cursor is not on valid entry'
+      end
+      vim.fn.chdir(vim.fs.dirname(path))
+      MiniFiles.open()
+    end
+
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesBufferCreate',
+      callback = function(args)
+        local b = args.data.buf_id
+        vim.keymap.set('n', 'g~', set_cwd, { buffer = b, desc = 'Set cwd' })
+      end,
+    })
+
     require('mini.statusline').setup {}
     require('mini.tabline').setup {}
     require('mini.indentscope').setup {
