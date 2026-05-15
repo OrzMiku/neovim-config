2b4151967b108a9cabdcd7c3f2bc231390e8aa07
OrzMiku <miku@ecy.pink>
2026-05-15 22:25:03 +0800

    more lsp intergrated

--- init.lua
+++ init.lua
@@ -16,21 +16,26 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+vim.opt.autocomplete = true
+vim.opt.complete:append("o")
+vim.opt.completeopt = "fuzzy,menuone,popup,noselect"
+vim.opt.pumborder = "single"
 
 require('vim._core.ui2').enable({
-  enable = true,
+    enable = true,
 })
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set({"n", "v"}, "<leader>Y", [["+Y]])
-vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
-vim.keymap.set({"n", "v"}, "<leader>P", [["+P]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
+vim.keymap.set({ "n", "v" }, "<leader>P", [["+P]])
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 vim.keymap.set("n", "<leader>Q", vim.diagnostic.setqflist)
 
 vim.api.nvim_create_autocmd('FileType', {
+    group = vim.api.nvim_create_augroup('UserTSAutoStart', { clear = true }),
     pattern = '*',
     callback = function(ev)
         local lang = ev.match
@@ -38,4 +43,14 @@ vim.api.nvim_create_autocmd('FileType', {
             vim.treesitter.start(ev.buf, lang)
         end
     end
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspCompletion', {}),
+    callback = function(ev)
+        local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+        if client:supports_method('textDocument/completion') then
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+        end
+    end,
 })
