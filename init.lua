88b73f182665b57a90ba7f7424cb7a19e1500757
OrzMiku <miku@ecy.pink>
2026-05-15 18:26:26 +0800

    fix fold, enable ui2, autostart treesitter hl

--- init.lua
+++ init.lua
@@ -10,12 +10,16 @@ vim.opt.list = true
 vim.opt.foldtext = ''
 vim.opt.foldlevel = 99
 vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'lua:vim.treesitter.foldexpr()'
-vim.opt.undofile = ture
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+
+require('vim._core.ui2').enable({
+  enable = true,
+})
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -25,3 +29,13 @@ vim.keymap.set({"n", "v"}, "<leader>p", [["+p]])
 vim.keymap.set({"n", "v"}, "<leader>P", [["+P]])
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 vim.keymap.set("n", "<leader>Q", vim.diagnostic.setqflist)
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = '*',
+    callback = function(ev)
+        local lang = ev.match
+        if vim.treesitter.language.add(lang) then
+            vim.treesitter.start(ev.buf, lang)
+        end
+    end
+})
